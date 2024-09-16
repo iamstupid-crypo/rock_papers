0f804c73ebf15806b944cdd755c7f934de5ca9ac
iamstupid-crypo <79710224+iamstupid-crypo@users.noreply.github.com>
2024-09-16 23:11:46 +0530

    Update rock_paper.py

--- rock_paper.py
+++ rock_paper.py
@@ -37,7 +37,7 @@ if user_choice==0 and computer_choice==2:
 
 elif computer_choice==0 and user_choice==2:
     print("you lose!")
-elif user_choice >= 3 or user_choice <= 0:
+elif user_choice >= 3 or user_choice < 0:
     print("you typed an invalid number. you lose!")
 elif computer_choice>user_choice:
     print("you lose :(")
